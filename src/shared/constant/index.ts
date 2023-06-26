50abcfdab778abfb0e2de9f998d94c298c016ecf
赵宏健 <757566833@qq.com>
2023-06-26 18:16:36 +0800

    feature address

--- src/shared/constant/index.ts
+++ src/shared/constant/index.ts
@@ -116,7 +116,7 @@ export const ADDRESS_TYPES: {
     value: AddressType.P2PKH,
     label: 'P2PKH',
     name: 'Legacy (P2PKH)',
-    hdPath: "m/44'/0'/0'/0",
+    hdPath: "m/44'/2'/0'/0",
     displayIndex: 3,
     isUnisatLegacy: false
   },
@@ -124,7 +124,7 @@ export const ADDRESS_TYPES: {
     value: AddressType.P2WPKH,
     label: 'P2WPKH',
     name: 'Native Segwit (P2WPKH)',
-    hdPath: "m/84'/0'/0'/0",
+    hdPath: "m/84'/2'/0'/0",
     displayIndex: 0,
     isUnisatLegacy: false
   },
@@ -132,7 +132,7 @@ export const ADDRESS_TYPES: {
     value: AddressType.P2TR,
     label: 'P2TR',
     name: 'Taproot (P2TR)',
-    hdPath: "m/86'/0'/0'/0",
+    hdPath: "m/86'/2'/0'/0",
     displayIndex: 2,
     isUnisatLegacy: false
   },
@@ -140,7 +140,7 @@ export const ADDRESS_TYPES: {
     value: AddressType.P2SH_P2WPKH,
     label: 'P2SH-P2WPKH',
     name: 'Nested Segwit (P2SH-P2WPKH)',
-    hdPath: "m/49'/0'/0'/0",
+    hdPath: "m/49'/2'/0'/0",
     displayIndex: 1,
     isUnisatLegacy: false
   },
@@ -148,7 +148,7 @@ export const ADDRESS_TYPES: {
     value: AddressType.M44_P2WPKH,
     label: 'P2WPKH',
     name: 'Native SegWit (P2WPKH)',
-    hdPath: "m/44'/0'/0'/0",
+    hdPath: "m/44'/2'/0'/0",
     displayIndex: 4,
     isUnisatLegacy: true
   },
@@ -156,7 +156,7 @@ export const ADDRESS_TYPES: {
     value: AddressType.M44_P2TR,
     label: 'P2TR',
     name: 'Taproot (P2TR)',
-    hdPath: "m/44'/0'/0'/0",
+    hdPath: "m/44'/2'/0'/0",
     displayIndex: 5,
     isUnisatLegacy: true
   }
@@ -165,7 +165,7 @@ export const ADDRESS_TYPES: {
 export const RESTORE_WALLETS: { value: RestoreWalletType; name: string; addressTypes: AddressType[] }[] = [
   {
     value: RestoreWalletType.UNISAT,
-    name: 'UniSat Wallet',
+    name: 'Litescribe Wallet',
     addressTypes: [
       AddressType.P2WPKH,
       AddressType.P2SH_P2WPKH,
@@ -200,7 +200,7 @@ export const RESTORE_WALLETS: { value: RestoreWalletType; name: string; addressT
 ];
 
 export const NETWORK_TYPES = [
-  { value: NetworkType.MAINNET, label: 'LIVENET', name: 'livenet', validNames: [0, 'livenet', 'mainnet'] },
+  { value: NetworkType.MAINNET, label: 'MAINNET', name: 'livenet', validNames: [0, 'livenet', 'mainnet'] },
   { value: NetworkType.TESTNET, label: 'TESTNET', name: 'testnet', validNames: ['testnet'] }
 ];
 
@@ -219,16 +219,17 @@ export const WALLETCONNECT_STATUS_MAP = {
   FAILD: 6
 };
 
-export const INTERNAL_REQUEST_ORIGIN = 'https://unisat.io';
+export const INTERNAL_REQUEST_ORIGIN = 'https://litescribe.io';
 
 export const INTERNAL_REQUEST_SESSION = {
-  name: 'UniSat Wallet',
+  name: 'Litescribe Wallet',
   origin: INTERNAL_REQUEST_ORIGIN,
   icon: './images/logo/logo@128x.png'
 };
 
-export const OPENAPI_URL_MAINNET = 'https://unisat.io/wallet-api-v4';
-export const OPENAPI_URL_TESTNET = 'https://unisat.io/testnet/wallet-api-v4';
+export const OPENAPI_URL_MAINNET = 'https://litescribe.io/api';
+// export const OPENAPI_URL_MAINNET = 'https://unisat.io/wallet-api-v4';
+export const OPENAPI_URL_TESTNET = 'https://testnet.litescribe.io/api';
 
 export const EVENTS = {
   broadcastToUI: 'broadcastToUI',
@@ -261,12 +262,14 @@ export const TO_LOCALE_STRING_CONFIG = {
   minimumFractionDigits: 8
 };
 
-export const SATS_DOMAIN = '.sats';
-export const UNISAT_DOMAIN = '.unisat';
+export const SATS_DOMAIN = '.lits';
+export const UNISAT_DOMAIN = '.unilit';
+export const LTC_DOMAIN = '.ltc';
+export const LITE_DOMAIN = '.lite';
 
-export const GITHUB_URL = 'https://github.com/unisat-wallet/extension';
-export const DISCORD_URL = 'https://discord.com/invite/EMskB2sMz8';
-export const TWITTER_URL = 'https://twitter.com/unisat_wallet';
+export const GITHUB_URL = 'https://github.com/ynohtna92/extension-ltc';
+export const DISCORD_URL = 'https://discord.gg/mGEPM6ECM2';
+export const TWITTER_URL = 'https://twitter.com/litescribe_io';
 
 export const CHANNEL = process.env.channel!;
 export const VERSION = process.env.release!;
